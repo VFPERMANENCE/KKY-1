1343d587f1ba8d844e0b19279954a3797520a37a
vfpermanence <vfpermanence@gmail.com>
2025-11-10 16:14:46 +0300

    end of the end stage 1

--- PW2/second.py
+++ PW2/second.py
@@ -3,25 +3,51 @@ import sys
 from urllib.parse import urlparse
 from pathlib import Path
 
+def validate_package_name(value):
+    if not value.strip():
+        raise argparse.ArgumentTypeError("Имя пакета не может быть пустым.")
+    return value
+
 def validate_url_or_path(value):
-    """Проверка: значение должно быть либо корректным URL, либо существующим путем."""
     parsed = urlparse(value)
     if parsed.scheme in ("http", "https"):
         return value
     p = Path(value)
     if not p.exists():
-        raise argparse.ArgumentTypeError(f"Путь '{value}' не существует.")
+        raise argparse.ArgumentTypeError(f"'{value}' не является корректным URL или путём к файлу.")
     return str(p.resolve())
 
 def validate_mode(value):
     allowed = ["local", "remote"]
     if value not in allowed:
-        raise argparse.ArgumentTypeError(f"Режим '{value}' не поддерживается. Используйте {allowed}.")
+        raise argparse.ArgumentTypeError(f"Режим '{value}' не поддерживается. Используйте: {', '.join(allowed)}.")
+    return value
+
+def validate_version(value):
+    if not value.strip():
+        raise argparse.ArgumentTypeError("Версия пакета не может быть пустой.")
+    # простейшая валидация вроде X.Y.Z
+    import re
+    if not re.match(r"^\d+(\.\d+){0,2}$", value): #регулярка - ^начало строки, \d+ одна или больше цифр,(\.\d+)
+        raise argparse.ArgumentTypeError("Версия должна быть в формате X.Y или X.Y.Z (например, 1.0.3).")
+    return value
+
+def validate_output(value):
+    if not value.strip():
+        raise argparse.ArgumentTypeError("Имя выходного файла не может быть пустым.")
+    if not any(value.endswith(ext) for ext in [".png", ".jpg", ".svg"]):
+        raise argparse.ArgumentTypeError("Имя выходного файла должно оканчиваться на .png, .jpg или .svg.")
+    return value
+
+def validate_filter(value):
+    if not isinstance(value, str):
+        raise argparse.ArgumentTypeError("Фильтр должен быть строкой.")
     return value
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Инструмент визуализации графа зависимостей (этап 1: конфигурация)"
+    parser = argparse.ArgumentParser (
+        description="Инструмент визуализации графа зависимостей",
+        epilog="Пример: python depgraph.py --package-name numpy --repo-url ./repo --mode local --version 1.0 --output graph.png"
     )
     
     parser.add_argument("--package-name", required=True, help="Имя анализируемого пакета.")
